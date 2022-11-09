7e65fe00ef4c1f6f0f91c793ca317b36e3bc877e
gamingflexer <osurve20ece@student.mes.ac.in>
2022-11-09 21:06:23 +0530

    sekf learner module added

---
+++ sourceparser/learner/learner.py
@@ -0,0 +1,14 @@
+import os
+import json
+from utils import captcha_verifer
+from config import data_path,resume_path,json_path
+
+# make data directory to store the resumes
+if os.path.exists(data_path) == False:
+    os.mkdir(data_path)
+    os.mkdir(resume_path)
+    os.mkdir(json_path)
+    
+def save_json(data, filename):
+    with open(os.path.join(json_path, filename), 'w') as f:
+        json.dump(data, f)
